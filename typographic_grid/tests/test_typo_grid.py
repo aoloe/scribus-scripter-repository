0609702cb7239ff072c2d090894dcab32d0fe9ff
Christophe Schockaert <r3vlibre@artlab.createcnix.lan>
2013-03-13 00:44:41 +0100

    Reorganize tests for better separation with original code.

--- typographic_grid/tests/test_typo_grid.py
+++ typographic_grid/tests/test_typo_grid.py
@@ -23,14 +23,79 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+# Load class and functions to be tested
 import imp
-typo_grid = imp.load_source("typographic_grid", "../typographic_grid.spy")
-print "Initialisation of module 'TypoGrid' =" + str(typo_grid)
+mod_typoGrid = imp.load_source("typographic_grid", "../typographic_grid.spy")
 
-if __name__ == '__main__':
-    typo_grid.run(
-        # Define test values (units = cm)
-        testValues = {
+
+class Test_ScribusTypoGridUI(object):
+    def __init__(self, gridSpec):
+        """Define grid specification as array describing 'page', 'margins', 'leading' and 'grid'"""
+        self.gridSpec = gridSpec
+        return
+
+    def getParamValues(self):
+        """Get values from test UI definition"""
+        # Get page specification from Scribus Document
+
+        return (self.gridSpec["page"], 
+                self.gridSpec["margins"],
+                self.gridSpec["leading"],
+                self.gridSpec["grid"])
+
+
+class Test_ScribusTypoGrid(mod_typoGrid.ScribusTypoGrid):
+    def __init__(self, ui):
+        super(Test_ScribusTypoGrid, self).__init__(ui)
+        return
+
+
+    def describeGridSpec(self):
+        """Generate a summary string of grid specifications"""
+        gridSpecs = [ ]
+        gridSpecs.append("Page:")
+        gridSpecs.append("  width = " + str(self.page["width"]))
+        gridSpecs.append("  height = " + str(self.page["height"]))
+        gridSpecs.append("")
+        gridSpecs.append("Margins:")
+        gridSpecs.append("  left = " + str(self.margins["left"]))
+        gridSpecs.append("  right = " + str(self.margins["right"]))
+        gridSpecs.append("  top = " + str(self.margins["top"]))
+        gridSpecs.append("  bottom = " + str(self.margins["bottom"]))
+        gridSpecs.append("")
+        gridSpecs.append("Leading interval:")
+        gridSpecs.append("  target = " + str(self.leading["target"]))
+        gridSpecs.append("  computed = " + str(self.leading["computed"]))
+        gridSpecs.append("  computed_lower = " + str(self.leading["computed_lower"]))
+        gridSpecs.append("  computed_upper = " + str(self.leading["computed_upper"]))
+        gridSpecs.append("")
+        gridSpecs.append("Grid:")
+        gridSpecs.append("  Limits:")
+        gridSpecs.append("    left = " + str(self.grid["left"]))
+        gridSpecs.append("    right = " + str(self.grid["right"]))
+        gridSpecs.append("    top = " + str(self.grid["top"]))
+        gridSpecs.append("    bottom = " + str(self.grid["bottom"]))
+        gridSpecs.append("    height = " + str(self.grid["height"]))
+        gridSpecs.append("    width = " + str(self.grid["width"]))
+        gridSpecs.append("    baseline_nb = " + str(self.grid["nb_baselines"]))
+        gridSpecs.append("  Vertical:")
+        gridSpecs.append("    nb_div = " + str(self.grid["vertical"]["nb_div"]))
+        gridSpecs.append("    gutter = " + str(self.grid["vertical"]["gutter"]))
+        gridSpecs.append("  Horizontal:")
+        gridSpecs.append("    nb_div = " + str(self.grid["horizontal"]["nb_div"]))
+        gridSpecs.append("    gutter = " + str(self.grid["horizontal"]["gutter"]))
+        return "\n".join(gridSpecs)
+
+
+    def createGrid(self):
+        print "createGrid: test routine -- no creation performed !"
+        return
+
+
+def run_tests():
+    ui_Test_001 = Test_ScribusTypoGridUI(
+        # Define test values for Test_001 (units = cm)
+        {
             # Page dimensions
             "page":
                 {"width": 21.0, "height": 29.7},
@@ -40,12 +105,19 @@ if __name__ == '__main__':
                  "top": 1.25, "bottom": 1.25},
             # Leading interval (cm)
             "leading":
-                {"initial": 0.77},
+                {"target": 0.77},
             # Grid specifications
             "grid":
                 {"vertical": {"nb_div": 6,
-                              "spacing": 0.8},
+                              "gutter": 0.8},
                  "horizontal": {"nb_div": 6,
-                                "spacing": 0.8}}
-        }
+                                "gutter": 0.8}}
+            }
         )
+    testGrid = mod_typoGrid.run(ui_Test_001)
+    GridSpec = testGrid.describeGridSpec()
+    print "Grid Specification:\n" + GridSpec
+
+
+if __name__ == '__main__':
+    run_tests()
